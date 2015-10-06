49000af1ccf6d03ce25100a3ce580664a8deffae
Gal <galcohen92@gmail.com>
2015-10-06 20:54:32 +0300

    1

---
+++ RecipeSite/Models/Category.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RecipeSite.Models
+{
+    public class Category
+    {
+        public int ID { get; set; }
+
+        public String name { get; set; }
+
+        public String imageUrl { get; set; }
+
+        public Category(){}
+
+        public Category(String name, String url)
+        {
+            this.imageUrl = url;
+            this.name = name;
+        }
+    }
+
+}
---
+++ RecipeSite/Models/RecipeView.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace RecipeSite.Models
+{
+    public class RecipeView
+    {
+
+        [Required]
+        [StringLength(30)]
+        public String title { get; set; }
+
+        [Required]
+        public String content { get; set; }
+
+        [DataType(DataType.Upload)]
+        public HttpPostedFileBase video { get; set; }
+
+        [Required]
+        public int likeAmount { get; set; }
+
+        //[Required]
+        //public IEnumerable<Ingredient> ingredients { get; set; }
+
+        [Required]
+        public IEnumerable<Category> categories { get; set; }
+
+        public virtual User author { get; set; }
+    }
+}
