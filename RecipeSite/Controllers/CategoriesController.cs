fbf61e6491ad4baef5880964aab62897eca3a84c
yardenDavidof <yardo.david@gmail.com>
2015-10-10 14:01:07 +0300

    upload image without view

--- RecipeSite/Controllers/CategoriesController.cs
+++ RecipeSite/Controllers/CategoriesController.cs
@@ -46,10 +46,19 @@ namespace RecipeSite.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "ID,name,imageUrl")] Category category)
+        public ActionResult Create([Bind(Include = "ID,name")] Category category, HttpPostedFileBase upload)
         {
             if (ModelState.IsValid)
             {
+                if (upload != null && upload.ContentLength > 0)
+                {
+                    string picName = System.IO.Path.GetFileName(upload.FileName);
+                    string picPath = System.IO.Path.Combine(
+                       Server.MapPath("~/Upload/Images/Categories"), picName);
+                    upload.SaveAs(picPath);
+                    category.imageUrl = "/Upload/Images/Categories/" + picName;
+                }
+
                 db.Categories.Add(category);
                 db.SaveChanges();
                 return RedirectToAction("Index");
