5b0b82a975a727ce5eb7d314ca25fc1df8ed769c
yardenDavidof <yardo.david@gmail.com>
2015-10-11 20:21:05 +0300

    categories

--- RecipeSite/Migrations/Configuration.cs
+++ RecipeSite/Migrations/Configuration.cs
@@ -14,18 +14,10 @@ namespace RecipeSite.Migrations
 
         protected override void Seed(RecipeSite.Models.RecipeSiteContext context)
         {
-            //  This method will be called after migrating to the latest version.
 
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
-            //  to avoid creating duplicate seed data. E.g.
-            //
-            //    context.People.AddOrUpdate(
-            //      p => p.FullName,
-            //      new Person { FullName = "Andrew Peters" },
-            //      new Person { FullName = "Brice Lambson" },
-            //      new Person { FullName = "Rowan Miller" }
-            //    );
-            //
+
+
+            
         }
     }
 }
